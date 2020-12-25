e3417e27c5cf078e1d9db062b2d8d8a0cdabc762
Lea Rosema <terabaud@gmail.com>
2020-12-25 16:48:14 +0100

    fix: expose API for watch mode

--- src/frontm8er.ts
+++ src/frontm8er.ts
@@ -1,5 +1,11 @@
 import { readDataFiles } from './utils/data-parser';
 import { processFrontmatterFiles } from './utils/file-processor';
+import { watchFrontmatterFiles } from './utils/file-watcher';
 import { MatterParser } from './utils/matter-parser';
 
-export { MatterParser, readDataFiles, processFrontmatterFiles };
+export {
+  MatterParser,
+  readDataFiles,
+  processFrontmatterFiles,
+  watchFrontmatterFiles,
+};
