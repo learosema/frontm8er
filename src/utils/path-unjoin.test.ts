13bb45351adf5f121a83c1e513e2f8742c8d7f8d
Lea Rosema <terabaud@gmail.com>
2020-12-25 13:33:17 +0100

    fix: fix test

--- src/utils/path-unjoin.test.ts
+++ src/utils/path-unjoin.test.ts
@@ -1,35 +1,34 @@
-import path from 'path';
 import { pathUnjoin } from './path-unjoin';
 
 describe('path.unjoin tests', () => {
   test('pathUnjoin with linux-style path without trailing slash', () => {
     const inputFolder = 'src/input';
-    const file = ['content', 'file.txt'].join(path.sep);
-    const joinedFile = path.join(inputFolder, file);
+    const file = 'content/file.txt';
+    const joinedFile = 'src/input/content/file.txt';
     const unjoined = pathUnjoin(joinedFile, inputFolder);
     expect(unjoined).toEqual(file);
   });
 
   test('pathUnjoin with linux-style path with trailing slash', () => {
     const inputFolder = 'src/input/';
-    const file = ['content', 'file.txt'].join(path.sep);
-    const joinedFile = path.join(inputFolder, file);
+    const file = 'content/file.txt';
+    const joinedFile = 'src/input/content/file.txt';
     const unjoined = pathUnjoin(joinedFile, inputFolder);
     expect(unjoined).toEqual(file);
   });
 
   test('pathUnjoin with windows-style path without trailing slash', () => {
     const inputFolder = 'src\\input';
-    const file = ['content', 'file.txt'].join(path.sep);
-    const joinedFile = path.join(inputFolder, file);
+    const file = 'content\\file.txt';
+    const joinedFile = 'src\\input\\content\\file.txt';
     const unjoined = pathUnjoin(joinedFile, inputFolder);
     expect(unjoined).toEqual(file);
   });
 
   test('pathUnjoin with windows-style path with trailing slash', () => {
     const inputFolder = 'src\\input\\';
-    const file = ['content', 'file.txt'].join(path.sep);
-    const joinedFile = path.join(inputFolder, file);
+    const file = 'content\\file.txt';
+    const joinedFile = 'src\\input\\content\\file.txt';
     const unjoined = pathUnjoin(joinedFile, inputFolder);
     expect(unjoined).toEqual(file);
   });
